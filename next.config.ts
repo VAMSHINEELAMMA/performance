8b6a1958a621fc6e32a89ccacdbd0dfb2770ee52
M H Vamshi Neelamma <neelammamhvamshi@gmail.com>
2025-09-06 13:25:55 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -25,6 +26,12 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'encrypted-tbn0.gstatic.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.boldbi.com',
         port: '',
         pathname: '/**',
       }
